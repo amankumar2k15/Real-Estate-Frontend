201b7edc42af02d556944d7513062758bfe60797
shashank8112357010 <shashanksharma1235999@gmail.com>
2023-12-29 10:40:32 +0530

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
+import { useEffect } from "react";
+import { getToken } from "./helper/tokenHelper";
+
 
 function App() {
+  const {pathname} = useLocation()
+  const token = getToken()
+  const navigate = useNavigate()
+  
+useEffect(()=>{
+if(!token) return navigate("/auth/sign-up")
+},[])
   return (
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
